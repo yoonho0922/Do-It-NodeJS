26fd35a21c1cf38a2345ff0e167daf7aa4a1e09e
yoonho <yoonho0922@naver.com>
2020-11-02 18:11:03 +0900

    ch05

---
+++ ch05/ch05_test1.js
@@ -0,0 +1,11 @@
+var http = require('http');
+
+// 웹 서버 객체 만들기
+var server = http.createServer();
+
+// 웹 서버를 시작하여 3000번 포트에서 대기
+var host = '172.16.169.47'
+var port = 3000;
+server.listen(port, host, function(){
+    console.log('웹 서버가 시작되었습니다 %d : ', port);
+});
---
+++ ch05/ch05_test7.js
@@ -0,0 +1,24 @@
+var http = require('http');
+
+var options = {
+    host: 'www.naver.com',
+    port: 80,
+    path: '/'
+};
+
+var req = http.get(options, function(res){
+    // 응답 처리
+    var resData = '';
+    
+    res.on('data', function(chunk){
+        resData += chunk;
+    });
+    
+    res.on('end', function(){
+        console.log(resData);
+    });
+});
+
+req.on('error', function(err){
+    console.log("오류 발생 : " + err.message);
+});
