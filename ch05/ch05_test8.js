a188c589fff24b5cab883c694ed5ef3434cdf7a9
yoonho <yoonho0922@naver.com>
2020-11-04 14:04:00 +0900

    ch05

---
+++ ch05/ch05_test8.js
@@ -0,0 +1,33 @@
+var http = require('http');
+
+var opts = {
+    host: 'www.naver.com',
+    port: 80,
+    method: 'POST',
+    path: '/'
+};
+
+var resData = '';
+
+var req = http.request(opts, function(res){
+    // 응답 처리
+    res.on('data', function(chunk){
+        resData += chunk;
+    });
+    
+    res.on('end', function(){
+        console.log(resData);
+    });
+});
+
+opts.headers['Content-Type'] = 'application/x-www-form-urlencoded';
+req.data = "q=actor";
+opts.headers['Content-Length'] = req.data.length;
+
+req.on('error', function(err){
+    console.log("오류 발생 : " + err.message);
+});
+
+// 요청 전송
+req.write(req.data);
+req.end();
