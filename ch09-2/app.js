089dd235e192c335e8e50146f9c4eb76977f27d1
yoonho <yoonho0922@naver.com>
2021-01-20 18:30:23 +0900

    refactor : 사용자 인증 모듈화

--- ch09-2/app.js
+++ ch09-2/app.js
@@ -57,78 +57,9 @@ app.use(passport.session());
 app.use(flash());
 
 //==== 패스포트 스트래티지 설정 ====//
-var LocalStrategy = require('passport-local').Strategy;
-
-// 패스포트 로그인 설정
-passport.use('local-login', new LocalStrategy({
-    usernameField : 'email',
-    passwordField : 'password',
-    passReqToCallback : true
-    }, function(req, email, password, done){
-    console.log('passport의 local-login 호출됨 : ' + email + ', ' + password);
-
-    var database = app.get('database');
-
-    database.UserModel.findOne({'email' : email}, function(err, user){
-        if(err) {return done(err)}
-
-        // 등록된 사용자가 없는 경우
-        if(!user){
-            console.log('계정이 일치하지 않음.');
-            return done(null, false, req.flash('loginMessage', '등록된 계정이 없습니다.'));
-        }
-
-        var authenticated = user.authenticate(password, user.salt, user.hashed_password);
-
-        // 비밀번호가 틀린 경우
-        if (!authenticated){
-            console.log('비밀번호 일치하지 않음.');
-            return done(null, false, req.flash('loginMessage', '비밀번호가 일치하지 않습니다.'));
-        }
-
-        console.log('계정과 비밀번호가 일치함.');
-        return done(null, user);
-    });
-}));
-
-// 패스포트 회원가입 설정
-passport.use('local-signup', new LocalStrategy({
-        usernameField : 'email',
-        passwordField : 'password',
-        passReqToCallback : true // 이 옵션을 설정하면 아래 콜백 함수의 첫번째 파라미터로 req 객체 전달됨
-    }, function(req, email, password, done){
-        // 요청 파라미터 중 name 파라미터 확인
-        console.dir(req)
-        console.dir(email)
-        console.dir(password)
-        console.dir(done)
-        var paramName = req.body.name || req.query.name;
-        console.log('passport의 local-signup 호출됨 : ' + email + ', ' + password + ', ' + paramName);
-
-        // User.findOne이 blocking되므로 async 방식으로 변경할 수도 있음
-        process.nextTick(function(){
-            var database = app.get('database');
-            database.UserModel.findOne({'email' : email}, function(err, user){
-                // 오류가 발생하면
-                if(err) {return done(err);}
-
-                // 기존에 이메일이 있다면
-                if(user){
-                    console.log('기존에 계정이 있음.');
-                    return done(null, false, req.flash('signupMessage', '계정이 이미 있습니다.'));
-                } else {
-                    // 모델 인스턴스 객체 만들어 저장
-                    var user = new database.UserModel({'email' : email, 'password' : password, name : paramName});
-                    console.log(user)
-                    user.save(function(err){
-                        if(err) { throw err;}
-                        console.log("사용자 데이터 추가함");
-                        return done(null, user);
-                    });
-                }
-            });
-        });
-    }));
+// 패스포트 설정
+var configPssport = require('./config/passport');
+configPssport(app, passport);
 
 // 사용자 인증에 성공했을 때 호출
 passport.serializeUser(function(user, done){
@@ -150,69 +81,9 @@ passport.deserializeUser(function(user, done){
 var router = express.Router()
 route_loader.init(app, router);
 
-// 홈 화면
-router.route('/').get(function(req, res){
-    console.log('/ 패스 요청됨.');
-    res.render('index.ejs');
-});
-
-// 로그인 폼 링크
-app.get('/login', function(req, res){
-    console.log('/login 패스 요청됨.');
-    res.render('login.ejs', {message : req.flash('loginMessage')});
-});
-
-app.post('/login', passport.authenticate('local-login', {
-    successRedirect : '/profile',
-    failureRedirect : 'login',
-    failureFlash : true
-}));
-
-
-// 회원가입 폼 링크
-app.get('/signup', function(req, res){
-    console.log('/signup 패스 요청됨.');
-    res.render('signup.ejs', {message : req.flash('signupMessage')});
-});
-
-app.post('/signup', passport.authenticate('local-signup', {
-    successRedirect : '/profile',
-    failureRedirect : '/signup',
-    failureFlash : true
-}));
-
-// 프로필 화면
-router.route('/profile').get(function(req, res){
-    console.log('/profile 패스 요청됨.');
-
-    // 인증된 경우 req.user 객체에 사용자 정보 있으며, 인증이 안 된 경우 req.user는 false 값임
-    console.log('req.user 객체의 값');
-    console.dir(req.user);
-
-    // 인증이 안된 경우
-    if(!req.user) {
-        console.log('사용자 인증이 안 된 상태임.');
-        res.redirect('/')
-        return;
-    }
-
-    // 인증된 경우
-    console.log('사용자 인증된 상태임.');
-    if (Array.isArray(req.user)){
-        res.render('profile.ejs', {user: req.user[0]});
-    } else {
-        res.render('profile.ejs', {user: req.user});
-    }
-});
-
-// 로그아웃
-app.get('/logout', function(req, res){
-    console.log('/logout 패스 요청됨.');
-    req.logout();
-    res.redirect('/');
-});
-
-
+// 패스포트 관련 함수 라우팅
+var userPassport = require('./routes/user_passport');
+userPassport(app, passport);
 
 //==== 에러 컨트롤 ====//
 var errorHandler = expressErrorHandler({
