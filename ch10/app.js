5b26fff6fcfcf4791c89d4a685ca9685666a20dc
yoonho <yoonho0922@naver.com>
2021-01-23 19:07:02 +0900

    feat : socket.io 서버 추가

--- ch10/app.js
+++ ch10/app.js
@@ -30,6 +30,9 @@ var route_loader = require('./routes/route_loader');
 // 패스포트 사용
 var passport = require('passport');
 var flash = require('connect-flash');
+// 채팅서버 관련 모듈
+var socketio = require('socket.io');
+var cors = require('cors');
 
 //==== 익스프레스 객체 생성 및 기본 설정 ====//
 var app = express();
@@ -45,6 +48,7 @@ app.use(expressSession({
     resave:true,
     saveUninitialized:true
 }));
+app.use(cors());
 
 //===== 뷰 엔진 설정 =====//
 app.set('views', __dirname + '/views');
@@ -70,21 +74,33 @@ userPassport(app, passport);
 //==== 에러 컨트롤 ====//
 var errorHandler = expressErrorHandler({
     static: {
-        '404' : './public/404.html'
+        '404': './public/404.html'
     }
 });
-app.all('*', function(req, res){
-    res.status(404).send('<h1>ERROR - 페이지를 찾을 수 없습니다.</h1>');
-})
 
-app.use(expressErrorHandler.httpError(404));
-app.use(errorHandler);
+app.use( expressErrorHandler.httpError(404) );
+app.use( errorHandler );
+
 
 //==== 서버 실행 ====//
-http.createServer(app).listen(app.get('port'), function(){
-    console.log('서버가 시작되었습니다. 포트 : ', app.get('port'));
+var server = http.createServer(app).listen(app.get('port'), function(){
+    console.log('서버가 시작되었습니다. 포트 : ' + app.get('port'));
 
     // 데이터베이스 초기화
     database.init(app, config);
+
+});
+
+//==== socket.io 서버 시작 ====//
+var io = socketio.listen(server);
+console.log('socket.io 요청을 받아들일 준비가 되었습니다.')
+
+//클라이언트가 연결했을 때의 이벤트 처리
+io.sockets.on('connection', function(socket) {
+    console.log('connection info :', socket.request.connection._peername);
+
+    // 소켓 객체에 클라이언트 Host, Port 정보 속성으로 추가
+    socket.remoteAddress = socket.request.connection._peername.address;
+    socket.remotePort = socket.request.connection._peername.port;
 });
 
